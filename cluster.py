3d037613e882a2790ae4de5f8af17cd2723516b5
Anirudh H M <anihm136@gmail.com>
2020-11-29 21:27:09 +0530

    Finish clustering and basic reporting

---
+++ cluster.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import unique
+
+import pickle
+from sklearn.cluster import DBSCAN
+
+a = pickle.load(open("func_vec_map.pkl", "rb"))
+b = pickle.load(open("vec_func_map.pkl", "rb"))
+
+X = []
+
+for file in a:
+    for func in a[file]:
+        X.append(a[file][func])
+
+X = np.array(X)
+
+# model = GaussianMixture(n_components=4)
+# model = MeanShift()
+# model = AgglomerativeClustering(n_clusters=i)
+# model = KMeans(n_clusters=5)
+model = DBSCAN()
+yhat = model.fit_predict(X)
+clusters = unique(yhat)
+function_clusters = []
+
+for i in clusters:
+    all_funcs = sum((b[tuple(func)] for func in X[yhat == i]), [])
+    function_clusters.append(all_funcs)
+    print(i, len(all_funcs))
+
+print("The following functions are found to be similar -")
+for idx, i in enumerate(function_clusters[1:], 1):
+    print(f"Cluster {idx}")
+    for j in i:
+        print(f"\t{j[0]}\t:\t{j[1]}")
+
+# for cluster in clusters:
+#     # get row indexes for samples with this cluster
+#     row_ix = np.where(yhat == cluster)
+#     print(len(row_ix[0]))
+#     # create scatter of these samples
+#     pyplot.scatter(X[row_ix, 0], X[row_ix, 1])
+# show the plot
+# plt.scatter(X[:, 0], X[:, 1], c=yhat, cmap="Paired")
+# plt.show()
+
+# tsne = TSNE()
+# X_2d = tsne.fit_transform(X)
+
+# plt.scatter(X_2d[:,0],X_2d[:,1])
+# plt.show()
--- main.py
+++ main.py
@@ -6,28 +6,31 @@ from functions import get_functions
 
 file_funcs = {}
 reverse_map = {}
-files = glob.glob("code_pairs_train/*.c")
+
+files = []
+
+for i in range(1, 9):
+    files += glob.glob(f"data/{i}*.c")
+
 tot = len(files)
+with open("missing_log.txt", "w") as log, open("used_log.txt", "w") as used:
+    for idx, filename in enumerate(files):
+        func_vectors = {}
+        print(f"{idx+1}/{tot}")
+        try:
+            func = get_functions(filename)
+            for f in func:
+                vec = get_vector(func[f], is_file=False)
+                func_vectors[f] = vec
+                try:
+                    reverse_map[tuple(vec)].append((filename, f))
+                except:
+                    reverse_map[tuple(vec)] = [(filename, f)]
 
-for idx, filename in enumerate(files):
-    func_vectors = {}
-    print(f"{idx+1}/{tot}")
-    try:
-        func = get_functions(filename)
-        for f in func:
-            vec = get_vector(func[f], is_file=False)
-            func_vectors[f] = vec
-            try:
-                reverse_map[tuple(vec)] = (filename, f)
-            except:
-                reverse_map[tuple(vec)] = [(filename, f)]
-
-        file_funcs[filename] = func_vectors
-    except:
-        pass
-
-print(file_funcs)
-print(reverse_map)
+            file_funcs[filename] = func_vectors
+            used.write(filename + "\n")
+        except:
+            log.write(filename + "\n")
 
 with open("func_vec_map.pkl", "wb") as f:
     pickle.dump(file_funcs, f)
